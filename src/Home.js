e294906e0a4196137e03db6f14dd0f444b98abf7
allisonkosborne <allisonkosborne@me.com>
2022-03-10 19:44:21 -0600

    css beginning

--- src/Home.js
+++ src/Home.js
@@ -6,24 +6,28 @@ export const Home = () => (
   <>
     <div className="hero-image">
       <div className="hero-text">
-        <h1>Welcome to Sown!</h1>
-        <p></p>
-        <PropsAndState yourName={"Allison"} />
-        <section className="section-content">
-          <button>Sow a Seed!</button>
-        </section>
+        <h1 className="welcome">Welcome to Sown!</h1>
+        <p>
+          <PropsAndState yourName={"Allison"} />
+          <section className="sow-button">
+            <button>Sow a Seed!</button>
+          </section>
+        </p>
 
-        <small>
-          "Seeds are everywhere yet nobody thinks of them, tiny bundles waiting
-          to be opened. Most any seed is small enough to hide in a locket, and
-          some are so small they get latched between two teeth. They are on our
-          forks, between our cracks of sidewalks and cobblestones of streets,
-          under trees, on trees, in the produce section of the supermarket. They
-          are in the wind, rolling along the desert, in our hair, in the fur of
-          animals, in our dogs' feet." - Janisse Ray
-        </small>
+        <div className="first-paragraph">
+          <small>
+            "Seeds are everywhere yet nobody thinks of them, tiny bundles
+            waiting to be opened. Most any seed is small enough to hide in a
+            locket, and some are so small they get latched between two teeth.
+            They are on our forks, between our cracks of sidewalks and
+            cobblestones of streets, under trees, on trees, in the produce
+            section of the supermarket. They are in the wind, rolling along the
+            desert, in our hair, in the fur of animals, in our dogs' feet." -
+            Janisse Ray
+          </small>
+        </div>
 
-        <div>
+        <div className="second-paragraph">
           Now is not the time to be silent. To remain stagnant. Now is the time
           to take our health, the health of our communities back into our sOWN
           hands. Our future, the future of our food, a sustenance that we each
--- src/components/PropsAndState.js
+++ src/components/PropsAndState.js
@@ -3,7 +3,7 @@ import React from "react";
 export const PropsAndState = ({ yourName }) => {
   return (
     <>
-      <h3>Welcome, {yourName}! </h3>
+      <h3 className="welcome-name">Welcome, {yourName}! </h3>
     </>
   );
 };
